14fa37613718799b7a2c3a287d75b3c9ae816a78
LucaBreebaart <luca.breebaart99@gmail.com>
2024-11-09 17:10:15 +0200

    Update next.config.mjs

--- formins/next.config.mjs
+++ formins/next.config.mjs
@@ -1,35 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.resolve = config.resolve || {};
-      config.resolve.fallback = config.resolve.fallback || {};
-      config.resolve.fallback.canvas = false;
-      
-      // Fix for PDF.js worker
-      config.module = config.module || {};
-      config.module.rules = config.module.rules || [];
-      config.module.rules.push({
-        test: /pdf\.worker\.(min\.)?js/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/[hash][ext][query]'
-        }
-      });
-  
-      return config;
-    },
-    experimental: {
-      serverActions: true,
-    },
-    images: {
-      domains: ['localhost'],
-    },
-    api: {
-      bodyParser: {
-        sizeLimit: '10mb',
-      },
-      responseLimit: '10mb',
-    },
-  };
-  
-  export default nextConfig;
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.fallback = config.resolve.fallback || {};
+    config.resolve.fallback.canvas = false;
+    
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.module.rules.push({
+      test: /pdf\.worker\.(min\.)?js/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/[hash][ext][query]'
+      }
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['localhost'],
+  }
+};
+
+export default nextConfig;
